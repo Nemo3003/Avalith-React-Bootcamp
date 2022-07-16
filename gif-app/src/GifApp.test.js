1ab8c6483bf144c133f156b476539dcac36d4c2b
Nemo3003 <ulimarian2000@gmail.com>
2022-07-16 20:38:23 -0300

    trying

--- gif-app/src/GifApp.test.js
+++ gif-app/src/GifApp.test.js
@@ -1,20 +1,40 @@
-import { GifApp } from "./GifApp";
+import { GifApp, handleAddCategory,handleReset } from "./GifApp";
 import { renderHook, act } from "@testing-library/react";
 //Verificar cambio en el estado de categorías, cuando se ejecuta la función handleAddCategory.
 describe('Testing the main component GifApp', () => {
 
     test('Debe retornar el estado inicial', () => {
-        const { result } = renderHook(() => GifApp());
-        expect(result.current.categorias).toEqual([]);
-    })
-
-    test('Debe agregar una categoría', () => {
-        const { result } = renderHook(() => GifApp());
-        act(() => {
-            result.current.handleAddCategory('One Punch');
+        const {result} = renderHook(()=>GifApp('One punch'));
+        expect(result.current.categories).toBeTruthy();
+    });
+    test ('Debe de verificar el cambio en el estado inicial', () => {
+        const {result} = renderHook(()=>GifApp('One punch'));
+        act(()=>{
+            result.current.handleAddCategory('One punch');
         }
         )
-        expect(result.current.categorias).toEqual(['One Punch']);
-    })
-});
+        expect(result.current.categories).toEqual(['One punch']);
+    });
+
+    
+}),
+describe('refreshPage', () => {
+    const { reload } = window.location;
+
+    beforeAll(() => {
+      Object.defineProperty(window, 'location', {
+        writable: true,
+        value: { reload: jest.fn() },
+      });
+    });
+
+    afterAll(() => {
+      window.location.reload = reload;
+    });
+
+    it('reloads the window', () => {
+      handleReset();
+      expect(window.location.reload).toHaveBeenCalled();
+    });
+  });
     
