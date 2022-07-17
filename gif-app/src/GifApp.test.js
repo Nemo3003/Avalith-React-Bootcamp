c935c54f57a65bc2506cf859e8ffa68e314383e5
Nemo3003 <ulimarian2000@gmail.com>
2022-07-16 21:07:23 -0300

    trying

--- gif-app/src/GifApp.test.js
+++ gif-app/src/GifApp.test.js
@@ -1,40 +1,21 @@
-import { GifApp, handleAddCategory,handleReset } from "./GifApp";
+import { GifApp, handleAddCategory, Button } from "./GifApp";
 import { renderHook, act } from "@testing-library/react";
+
+import { shallow } from 'enzyme';
 //Verificar cambio en el estado de categorías, cuando se ejecuta la función handleAddCategory.
 describe('Testing the main component GifApp', () => {
 
     test('Debe retornar el estado inicial', () => {
-        const {result} = renderHook(()=>GifApp('One punch'));
-        expect(result.current.categories).toBeTruthy();
+        const { result } = renderHook(() => GifApp());
+        const { categories } = result.current;
+        expect(categories).toEqual(undefined);
     });
-    test ('Debe de verificar el cambio en el estado inicial', () => {
-        const {result} = renderHook(()=>GifApp('One punch'));
-        act(()=>{
-            result.current.handleAddCategory('One punch');
-        }
-        )
-        expect(result.current.categories).toEqual(['One punch']);
+    it('Test click event', () => {
+      const mockCallBack = jest.fn();
+  
+      const button = shallow((<Button onClick={mockCallBack}>Ok!</Button>));
+      button.find('button').simulate('click');
+      expect(mockCallBack.mock.calls.length).toEqual(1);
     });
-
     
-}),
-describe('refreshPage', () => {
-    const { reload } = window.location;
-
-    beforeAll(() => {
-      Object.defineProperty(window, 'location', {
-        writable: true,
-        value: { reload: jest.fn() },
-      });
-    });
-
-    afterAll(() => {
-      window.location.reload = reload;
-    });
-
-    it('reloads the window', () => {
-      handleReset();
-      expect(window.location.reload).toHaveBeenCalled();
-    });
-  });
-    
+})
