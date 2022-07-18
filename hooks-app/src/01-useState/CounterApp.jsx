49ee1f7524ba4b712bacff1296775db6e1338e6e
Nemo3003 <ulimarian2000@gmail.com>
2022-07-18 14:52:33 -0300

    hooks

--- hooks-app/src/01-useState/CounterApp.jsx
+++ hooks-app/src/01-useState/CounterApp.jsx
@@ -10,11 +10,28 @@ export const CounterApp = () => {
         }
     )
     const {counter1, counter2, counter3} = counter
+    //<button className='btn btn-primary' onClick={()=>{setCounter({...counter,counter1:counter1 +1})}}>+1</button> 
+    const handleCounter = () =>{
+        if(counter3>=30)return
+        let counterName = 'counter1';
+        if(counter1 >= 10) counterName = 'counter2';
+        if(counter2 >= 20) counterName = 'counter3';
+        setCounter({
+            ...counter,
+            [counterName]: counter[counterName] + 1}
+
+        )
+    }
     return(
         <>
         <h2>Counter App</h2>
         <h3>Counter: { counter1 }</h3>
-        <button className='btn btn-primary' onClick={()=>{setCounter({...counter,counter1:counter1 +1})}}>+1</button> 
+        <h3>Counter: {counter2}</h3>
+        <h3>Counter: {counter3}</h3>
+        {//Tiene que sumar counter1 hasta el 10 y luego seguir con counter2 hasta 20 y luego continuar en el counter3}
+        }
+        <button className='btn btn-primary' onClick={handleCounter}>+1</button>
+
         </>
     )
 }
